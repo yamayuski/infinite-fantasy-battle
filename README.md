6ea35c9228cc948d2ba472ba5bb03ced802de5d8
yamayuski <m.yamagishi90+git@gmail.com>
2024-09-08 10:49:34 +0900

    feat: work with docker compose

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # Infinite FANTASY BATTLE simulator
 
+## Install
+
+Add `ifb.test` and `api.ifb.test` to hosts file
+
+Install mkcert
+
+```ps1
+# in host computer
+mkcert ifb.test "*.ifb.test"
+```
+
+copy pem file to `/docker/ingress/cert.pem` and `/docker/ingress/key.pem`.
+
+```sh
+docker compose up -d
+```
+
 ## License
 
 See [LICENSE](LICENSE).
---
+++ backend/worker.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Shibare\HttpServer\RoadRunnerHttpDispatcher;
+use Shibare\HttpServer\RoutingHandler;
+use Shibare\HttpMessage\Response;
+use Shibare\Log\Formatters\JsonLineFormatter;
+use Shibare\Log\Logger;
+use Shibare\Log\Writers\StderrWriter;
+
+(static function (): void {
+    $dispatcher = new RoadRunnerHttpDispatcher();
+    $logger = new Logger([
+        new StderrWriter(new JsonLineFormatter()),
+    ]);
+
+    $handler = new RoutingHandler();
+    $handler->get('/', new class () implements RequestHandlerInterface {
+        public function handle(ServerRequestInterface $request): ResponseInterface
+        {
+            return new Response();
+        }
+    }, []);
+
+    $dispatcher->serve($logger, $handler);
+})();
