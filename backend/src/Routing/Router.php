6934292b95368e59eb336cdee632b3bfe39bf11f
yamayuski <m.yamagishi90+git@gmail.com>
2024-09-08 12:48:34 +0900

    feat: Add yaml extension

---
+++ backend/src/Routing/Router.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @license MIT
+ */
+
+namespace Ifb\Routing;
+
+class Router
+{
+
+}
--- backend/worker.php
+++ backend/worker.php
@@ -2,6 +2,10 @@
 
 declare(strict_types=1);
 
+/**
+ * @license MIT
+ */
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Psr\Http\Message\ServerRequestInterface;
